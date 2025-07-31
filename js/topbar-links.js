e100031abefbf23b3dd81e2e2c9c9a9f0d0d592a
kedoshim <abraaodpaula@gmail.com>
2025-07-31 15:19:01 -0300

    Css changes

--- js/topbar-links.js
+++ js/topbar-links.js
@@ -1,6 +1,6 @@
 // kedoshim.github.io/Catalogo-AmoVoce/
 export const links = [
-    { label: "Catálogo", path: "kedoshim.github.io/Catalogo-AmoVoce/" },
+    { label: "Catálogo", path: "https://kedoshim.github.io/Catalogo-AmoVoce/" },
     // { label: "Dia dos Namorados", path: "../pages/namorados.html"},
     { label: "Dia dos Pais", path: "./pages/pais-25.html" },
     { label: "Instruções", path: "#end" },
