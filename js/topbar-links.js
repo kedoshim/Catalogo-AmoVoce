03f5e8ba82e598d0b7fccfac1b5cc6ed019fe653
kedoshim <abraaodpaula@gmail.com>
2025-07-30 22:46:24 -0300

    Change pais link

--- js/topbar-links.js
+++ js/topbar-links.js
@@ -1,7 +1,7 @@
 // kedoshim.github.io/Catalogo-AmoVoce/
 export const links = [
-    { label: "Catálogo", path: "../index.html" },
+    { label: "Catálogo", path: "kedoshim.github.io/Catalogo-AmoVoce/" },
     // { label: "Dia dos Namorados", path: "../pages/namorados.html"},
-    { label: "Dia dos Pais", path: "../pages/pais-25.html" },
+    { label: "Dia dos Pais", path: "./pages/pais-25.html" },
     { label: "Instruções", path: "#end" },
 ];
