1a9df993b588abb8e32771da3bcf784d1d412d0f
Abraão de Paula <84100271+kedoshim@users.noreply.github.com>
2025-12-07 16:16:00 -0300

    Add Natal page prototype

---
+++ products/products-natal-25.js
@@ -0,0 +1,29 @@
+export const products = [
+    {
+        title: "Báu Natalino",
+        price: "179,90",
+        description: "",
+        items: "báu de madeira • garrafa de vinho • mini panettone • chocolate ao leite • esfera recheada com castanhas • peça de queijo parmesão • nozes selecionadas • decoração natalina exclusiva • fechamento em celofane transparente com fita de cetim • cartão temático para sua mensagem especial",
+        images: {
+            1: "../img/natal-25/caixa.jpg",
+        },
+    },
+    {
+        title: "Carta Natalina",
+        price: "99,90",
+        description: "",
+        items: "flor • mini panetone • caixa envelope • caixa Ferreiro Rocher com 3 unidades",
+        images: {
+            1: "../img/natal-25/carta.jpg",
+        },
+    },
+    {
+        title: "Box Natalino",
+        price: "99,90",
+        description: "Disponível para pronta entrega 🌟",
+        items: "Cesta mdf 25x15 reutilizável • Suco • Cafe individual - DripCoffee • Croissant recheado • Torradas • Mini baquete com parmesão • Polenguinho • Frios (queijo prato/ peito de peru) • Salada de frutas • Bolo no pote de coco • Cartão de felicitações",
+        images: {
+            1: "../img/natal-25/box.jpg",
+        },
+    },
+];
