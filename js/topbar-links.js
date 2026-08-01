a914c40617548731126024049a2a5778ef1d144c
Abraão de Paula <84100271+kedoshim@users.noreply.github.com>
2026-08-01 09:11:55 -0300

    Update topbar-links.js

--- js/topbar-links.js
+++ js/topbar-links.js
@@ -1,6 +1,6 @@
 export const links = [
     { label: "Catálogo", path: "https://kedoshim.github.io/Catalogo-AmoVoce/" },
-    { label: "Dia dos Namorados", path: "./pages/namorados-26.html" },
+    // { label: "Dia dos Namorados", path: "./pages/namorados-26.html" },
     { label: "Dia dos Pais 2026", path: "./pages/pais-26.html" },
     { label: "Instruções", path: "#end" },
 ];
